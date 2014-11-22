3e48559fdffb024a37c452eac3dde5f9e801404a
szmitas <szmitas@ubuntu>
2014-11-22 09:52:34 +0100

    brakowało $type w singletonie

--- Framework/FDbConnection.php
+++ Framework/FDbConnection.php
@@ -2,29 +2,29 @@
 
 class FDbConnection {
 
-	private static $singleton;
-	private static $db_name;
-	public $PDOInstance;
+    private static $singleton;
+    private static $db_name;
+    public $PDOInstance;
 
-        // Jak dla mnie wystarczy podac argumenty jak do PDO, driver, user i password,
-        // ale nie wnikam
-	private function __construct($db_type, $host, $db_name, $db_user, $db_password, $db_charset, $db_collate ) {
-		$this->db_name = $db_name;
-        	$this->PDOInstance = new FPDO( $db_type . ":host=" . $host . ";dbname=" . $db_name, $db_user, $db_password );
-		$this->PDOInstance->query( "SET NAMES " . $db_charset . " COLLATE " . $db_collate );
-		$this->PDOInstance->query( "SET time_zone = 'Europe/Warsaw'" );
-		//$this->PDOInstance->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
-	}
+    // Jak dla mnie wystarczy podac argumenty jak do PDO, driver, user i password,
+    // ale nie wnikam
+    private function __construct($db_type, $host, $db_name, $db_user, $db_password, $db_charset, $db_collate) {
+        $this->db_name = $db_name;
+        $this->PDOInstance = new FPDO($db_type . ":host=" . $host . ";dbname=" . $db_name, $db_user, $db_password);
+        $this->PDOInstance->query("SET NAMES " . $db_charset . " COLLATE " . $db_collate);
+        $this->PDOInstance->query("SET time_zone = 'Europe/Warsaw'");
+        //$this->PDOInstance->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+    }
 
-	public static function instance( $host, $db_name, $db_user, $db_password, $db_charset, $db_collate ) {
-		if ( !(self::$singleton instanceof self) || self::$singleton->db_name !== $db_name ) {
-			self::$singleton = new self( $host, $db_name, $db_user, $db_password, $db_charset, $db_collate );
-		}
-		return self::$singleton;
-	}
+    public static function instance($db_type, $host, $db_name, $db_user, $db_password, $db_charset, $db_collate) {
+        if (!(self::$singleton instanceof self) || self::$singleton->db_name !== $db_name) {
+            self::$singleton = new self($db_type, $host, $db_name, $db_user, $db_password, $db_charset, $db_collate);
+        }
+        return self::$singleton;
+    }
 
-	public static function get() {
-		return self::instance()->PDOInstance;
-	}
+    public static function get() {
+        return self::instance()->PDOInstance;
+    }
 
 }

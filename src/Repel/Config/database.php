6e275fff3dc8ad45e6f3e81f715f74062af59d96
Zhaala <Zhaala@192.168.1.190>
2014-12-17 20:13:57 +0100

    config podzial na connection

--- src/Repel/Config/database.php
+++ src/Repel/Config/database.php
@@ -1,14 +1,18 @@
 <?php
+
 return array(
-    'driver' => 'pgsql:dbname=szmitas_lbn;host=localhost',
-    'type' => 'pgsql',
-    'database' => 'szmitas_lbn',
-    'host' => 'localhost',
-//    'username' => 'postgres',
-//    'password' => 'trebiras',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-    'collate' => 'utf8_polish_ci',
-    'schema' => '../_sql/schema.sql'
+    'primary' => array(
+        'driver' => 'pgsql:dbname=test;host=localhost',
+        'type' => 'pgsql',
+        'database' => 'test',
+//    'database' => 'szmitas_lbn',
+        'host' => 'localhost',
+        'username' => 'postgres',
+        'password' => 'trebiras',
+//    'username' => 'root',
+//    'password' => 'root',
+        'charset' => 'utf8',
+        'collate' => 'utf8_polish_ci',
+        'schema' => '../_sql/schema.sql'
+    )
 );

ac9ed8dec1f3f0ab6110af1d646e6047f0cc96f1
Zhaala <Zhaala@192.168.1.190>
2014-12-17 01:11:50 +0100

    Adapter - Uporządkowałem fetchery, zostały generatory

--- bin/init.php
+++ bin/init.php
@@ -11,17 +11,11 @@ $config = require_once 'private_config.php';
 try {
     // Create Instance and connect
     $adapter = new Adapter($config);
-    $adapter
-//            ->addFetcher(new Fetcher\PostgreSQLFetcher($config))
+    $adapter->addFetcher(new Fetcher\PostgreSQLFetcher('primary'))
+            ->addFetcher(new Fetcher\PhpManyToManyFetcher(__DIR__.'/../relationships.php'))
             ->fetch()
             ->save();
 } catch (Exception $ex) {
     echo CLI::failure($ex);
     die();
 }
-//
-//$adapter = new Adapter($config);
-//$adapter->addFetcher(new \Repel\Adapter\Fetcher\PostgreSQLFetcher($db))
-//        ->fetch()
-//        ->addGenerator()
-//        ->generate();
